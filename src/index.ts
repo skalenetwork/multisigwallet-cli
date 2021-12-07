a94a5c79318b78b9787a1d3ec3df1f20c490f2b8
Vadim Yavorsky <yavrsky@gmail.com>
2021-12-07 19:26:36 +0400

    Init commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+
+import { Command } from 'commander'
+
+async function main() {
+  const program = new Command()
+
+  program
+    .argument('<function>', 'Function that you wanna call on MultiSigWallet contract')
+    .option(
+      '-f, --function <name>',
+      'Function that you wanna call on MultiSigWallet contract'
+    )
+    .parse()
+      
+  const url = program.args
+  console.log(url)
+  const options = program.opts()
+      
+  console.log(`url: ${url}, iteration: ${options.iteration}`)
+}
+
+if (require.main === module) {
+    main()
+        .then(() => process.exit(0))
+        .catch(error => {
+            console.error(error);
+            process.exit(1);
+        });
+}
