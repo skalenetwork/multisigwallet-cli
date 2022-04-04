7298f488bd60028bf9064544b064f5236916ac58
Vadim Yavorsky <yavrsky@gmail.com>
2022-04-04 21:10:04 +0300

    Add checks for python version

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
-python3 -m venv venv
+
+if [ $(python -V | cut -d ' ' -f 2) ] ; then
+    VERSION=$(python -V | cut -d ' ' -f 2)
+    MAJOR=$(echo ${VERSION::3} | cut -d '.' -f 1)
+    MINOR=$(echo ${VERSION::3} | cut -d '.' -f 2)
+    if [ $MAJOR = 3 ] && [ $MINOR -ge 7 ] ; then
+        python -m venv venv
+    fi
+elif [ $(python3 -V | cut -d ' ' -f 2) ] ; then
+    VERSION=$(python3 -V | cut -d ' ' -f 2)
+    MAJOR=$(echo ${VERSION::3} | cut -d '.' -f 1)
+    MINOR=$(echo ${VERSION::3} | cut -d '.' -f 2)
+    if [ $MAJOR = 3 ] && [ $MINOR -ge 7 ] ; then
+        python3 -m venv venv
+    fi
+else 
+    echo "Setup failed. Requires: Python>=3.7"
+    exit 1
+fi
+
 source venv/bin/activate
 pip install -r scripts/requirements.txt
-python3 scripts/generate_abi.py
+python scripts/generate_abi.py
 wget -O ./data/ima_mainnet.json https://raw.githubusercontent.com/skalenetwork/skale-network/master/releases/mainnet/IMA/1.1.0/abi.json
