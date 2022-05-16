7c179a64b5dfb8c96f5ccfaf199356fb6e2cf96a
Vadim Yavorsky <yavrsky@gmail.com>
2022-05-16 12:42:49 +0300

    Use recommended way of pip

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -22,7 +22,7 @@ fi
 
 source venv/bin/activate
 python3 -m pip install -r scripts/requirements.txt
-python scripts/generate_abi.py
+python3 scripts/generate_abi.py
 git clone https://github.com/skalenetwork/skale-network.git
 STABLE_IMA_VERSION=$(ls skale-network/releases/mainnet/IMA/ | sort -r | head -n 1)
 cp skale-network/releases/mainnet/IMA/$STABLE_IMA_VERSION/mainnet/abi.json data/ima_mainnet.json
